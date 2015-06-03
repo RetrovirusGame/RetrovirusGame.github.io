dbac67595096cf16fc1c05a27bcf6982d1a6efae
Chaiyawat Nunes <thaiberius.code@gmail.com>
2015-06-02 20:30:48 -0700

    Keypress instead of keydown

--- main.js
+++ main.js
@@ -99,7 +99,7 @@ function init() { // Main function
 	
 	// ========== CRUCIAL FUNDEMENTALS v ==========
 	
-	window.addEventListener('keydown', function(event) { // Key listener
+	window.addEventListener('keypress', function(event) { // Key listener
     	switch (event.keyCode) {
   			case 37: // Left
   				if(virus.x < 0) {} // For edge collision
