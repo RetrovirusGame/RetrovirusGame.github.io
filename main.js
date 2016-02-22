44dc2645e37dafe2c8d200f8b285e752a4b10141
Chai Nunes <chaiteanunes@gmail.com>
2016-02-21 20:03:33 -0800

    Update main.js

--- main.js
+++ main.js
@@ -73,7 +73,7 @@ function init() { // Main function
         }
     }
     yCount -= 1
-    grid(gridCount) // 20 squares across
+    grid(Math.round(cW / 16)) // 20 squares across
     
     /////////////////////// CLASSES ///////////////////////
     
